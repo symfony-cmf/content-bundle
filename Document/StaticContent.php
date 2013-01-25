01908fd148f632ef71d27a84afa2d271c0f58d8c
Alain Horner <alain.horner@liip.ch>
2013-01-25 15:36:00 +0100

    removed publish workflow specific stuff from content documents (moved to RoutingExtraBundle)

--- Document/StaticContent.php
+++ Document/StaticContent.php
@@ -5,12 +5,11 @@ namespace Symfony\Cmf\Bundle\ContentBundle\Document;
 use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
 
 use Symfony\Cmf\Component\Routing\RouteAwareInterface;
-use Symfony\Cmf\Bundle\CoreBundle\PublishWorkflow\PublishWorkflowInterface;
 
 /**
  * @PHPCRODM\Document(referenceable=true)
  */
-class StaticContent implements RouteAwareInterface, PublishWorkflowInterface
+class StaticContent implements RouteAwareInterface
 {
     /**
      * to create the document at the specified location. read only for existing documents.
@@ -48,16 +47,6 @@ class StaticContent implements RouteAwareInterface, PublishWorkflowInterface
      * @PHPCRODM\String(multivalue=true)
      */
     protected $tags = array();
-
-    /**
-     * @PHPCRODM\Date()
-     */
-    protected $publishStartDate;
-
-    /**
-     * @PHPCRODM\Date()
-     */
-    protected $publishEndDate;
 
     /**
      * This will usually be a ContainerBlock but can be any block that will be
@@ -134,32 +123,6 @@ class StaticContent implements RouteAwareInterface, PublishWorkflowInterface
     public function setTags($tags)
     {
         $this->tags = $tags;
-    }
-
-    /**
-     * Get the publish start date
-     */
-    public function getPublishStartDate()
-    {
-        return $this->publishStartDate;
-    }
-
-    public function setPublishStartDate(\DateTime $publishStartDate = null)
-    {
-        $this->publishStartDate = $publishStartDate;
-    }
-
-    /**
-     * Get the publish end date
-     */
-    public function getPublishEndDate()
-    {
-        return $this->publishEndDate;
-    }
-
-    public function setPublishEndDate(\DateTime $publishEndDate = null)
-    {
-        $this->publishEndDate = $publishEndDate;
     }
 
     /**
