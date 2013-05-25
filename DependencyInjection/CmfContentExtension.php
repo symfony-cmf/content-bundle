b6b073d62cf31cfe043f6e48332ae174fb01fcde
dantleech <dan.t.leech@gmail.com>
2013-05-25 16:24:52 +0100

    [WIP] Removed symfony_cmf prefix

--- DependencyInjection/CmfContentExtension.php
+++ DependencyInjection/CmfContentExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Definition\Processor;
 
-class SymfonyCmfContentExtension extends Extension
+class CmfContentExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
