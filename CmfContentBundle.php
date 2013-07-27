79b0a6375f7730f0ab81a1a4d27454c408f8c427
dantleech <dan.t.leech@gmail.com>
2013-07-27 15:14:56 +0100

    Refactored to model Model

--- CmfContentBundle.php
+++ CmfContentBundle.php
@@ -6,4 +6,18 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CmfContentBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        if (class_exists('Doctrine\Bundle\PHPCRBundle\DependencyInjection\Compiler\DoctrinePhpcrMappingsPass')) {
+            $container->addCompilerPass(
+                DoctrinePhpcrMappingsPass::createXmlMappingDriver(
+                    array(
+                        realpath(__DIR__ . '/Resources/config/doctrine-model') => 'Symfony\Cmf\Bundle\MenuBundle\Model',
+                        realpath(__DIR__ . '/Resources/config/doctrine-phpcr') => 'Symfony\Cmf\Bundle\MenuBundle\Doctrine\Phpcr',
+                    ),
+                    array('cmf_menu.document_manager_name')
+                )
+            );
+        }
+    }
 }
--- DependencyInjection/CmfContentExtension.php
+++ DependencyInjection/CmfContentExtension.php
@@ -16,40 +16,37 @@ class CmfContentExtension extends Extension
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
+        $keys = array(
+            'document_class', 
+            'document_manager_name', 
+            'default_template',
+            'content_basepath',
+            'static_basepath',
+        );
+
+        foreach ($keys as $key) {
+            if (isset($config[$key])) {
+                $container->setParameter($this->getAlias() . '.'.$key, $config[$key]);
+            }
+        }
+
         if ($config['use_sonata_admin']) {
             $this->loadSonataAdmin($config, $loader, $container);
         }
 
         if (isset($config['multilang'])) {
-            if ($config['multilang']['use_sonata_admin']) {
-                $this->loadSonataAdmin($config['multilang'], $loader, $container, 'multilang.');
-            }
-            if (isset($config['multilang']['document_class'])) {
-                $container->setParameter($this->getAlias() . '.multilang.document_class', $config['multilang']['document_class']);
-            }
-
             $container->setParameter($this->getAlias() . '.multilang.locales', $config['multilang']['locales']);
         }
-
-        if (isset($config['document_class'])) {
-            $container->setParameter($this->getAlias() . '.document_class', $config['document_class']);
-        }
-        if (isset($config['default_template'])) {
-            $container->setParameter($this->getAlias() . '.default_template', $config['default_template']);
-        }
-
-        $container->setParameter($this->getAlias() . '.content_basepath', $config['content_basepath']);
-        $container->setParameter($this->getAlias() . '.static_basepath', $config['static_basepath']);
     }
 
-    public function loadSonataAdmin($config, XmlFileLoader $loader, ContainerBuilder $container, $prefix = '')
+    public function loadSonataAdmin($config, XmlFileLoader $loader, ContainerBuilder $container)
     {
         $bundles = $container->getParameter('kernel.bundles');
         if ('auto' === $config['use_sonata_admin'] && !isset($bundles['SonataDoctrinePHPCRAdminBundle'])) {
             return;
         }
         
-        $loader->load($prefix . 'admin.xml');
+        $loader->load('admin.xml');
 
         if (isset($config['admin_class'])) {
             $container->setParameter($this->getAlias() . '.' . $prefix . 'admin_class', $config['admin_class']);
