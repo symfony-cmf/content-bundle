e39a155a43c06900e0da33ef694bbd2591edfb00
Wouter J <waldio.webdesign@gmail.com>
2013-12-27 15:56:11 +0100

    Added missing license headers

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,6 +9,7 @@
  * file that was distributed with this source code.
  */
 
+
 namespace Symfony\Cmf\Bundle\ContentBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
