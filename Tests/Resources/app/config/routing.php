2ad7f263e9fe4fce542f873d17c9ff6fcf95d727
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-05-11 00:09:26 +0200

    remove routing.yml from test app

--- Tests/Resources/app/config/routing.php
+++ Tests/Resources/app/config/routing.php
@@ -15,12 +15,6 @@ $collection = new RouteCollection();
 $collection->addCollection(
     $loader->import(CMF_TEST_CONFIG_DIR.'/routing/sonata_routing.yml')
 );
-
-
-//$collection->addCollection(
-//    $loader->import(__DIR__ . '/routing.yml')
-//);
-
 // $collection->addCollection(
 //     $loader->import(__DIR__.'/routing/my_test_routing.yml')
 // );
