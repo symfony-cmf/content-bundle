2d1f28160cb1fc2f8273c8e481a45a727ae42644
ElectricMaxxx <maximilian.berghoff@gmx.de>
2017-11-06 09:01:38 +0100

    fixed due to hints

--- tests/Fixtures/App/config/bundles.php
+++ tests/Fixtures/App/config/bundles.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2017 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Knp\Bundle\MenuBundle\KnpMenuBundle;
 use Symfony\Cmf\Bundle\ContentBundle\CmfContentBundle;
 use Symfony\Cmf\Bundle\CoreBundle\CmfCoreBundle;
